ff215a00141325c9506de13b45c70d6117459045
hsg5533 <hsg5533@naver.com>
2025-01-10 10:57:14 +0900

    change db engine

--- chatAppProject/settings.py
+++ chatAppProject/settings.py
@@ -91,10 +91,19 @@ WSGI_APPLICATION = 'chatAppProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chatapp',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
+    },
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
